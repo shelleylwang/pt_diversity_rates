251f8135b4aa98d29051c4006c6ff5e6898caee7
shelleylwang <seashellsw@gmail.com>
2024-11-05 13:01:39 -0500

    edits to sbatch params to make slurm script run faster

--- pyrate/bdnn_slurm.sh
+++ pyrate/bdnn_slurm.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --job-name=BDNN_nopreds       # create a short name for your job
-#SBATCH --nodes=1                 # node count
+#SBATCH --nodes=2                 # node count
 #SBATCH --ntasks=30                # total number of tasks across all nodes
-#SBATCH --cpus-per-task=1         # cpu-cores per task (>1 if multi-threaded tasks)
-#SBATCH --mem-per-cpu=4G          # memory per cpu-core (4G is default)
+#SBATCH --cpus-per-task=2         # cpu-cores per task (>1 if multi-threaded tasks)
+#SBATCH --mem-per-cpu=6G          # memory per cpu-core (4G is default)
 #SBATCH --time=1-01:00:00       # days-hh:mm:ss
 #SBATCH --mail-type=begin         # send email when job begins
 #SBATCH --mail-type=end           # send email when job ends
