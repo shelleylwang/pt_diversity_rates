bdd0910df6d1df9bbac845ab1c70a6b42c5a6f7f
shelleylwang <seashellsw@gmail.com>
2024-11-15 11:13:39 -0500

    editing bdnn_rep_nopreds_test into syn 256 test for cluster

--- pyrate/bdnn_syn_nopreds_TEST_256.sh
+++ pyrate/bdnn_syn_nopreds_TEST_256.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#SBATCH --job-name=BDNN_rep_nopreds_TEST_256    # Job name
+#SBATCH --job-name=BDNN_syn_nopreds_TEST_256    # Job name
 #SBATCH --ntasks=1          # Each BDNN analysis runs as a single task
 #SBATCH --mem-per-cpu=256G            # Memory per CPU core
 #SBATCH --time=1-00:00:00       # days-hh:mm:ss
 #SBATCH --mail-type=begin         # send email when job begins
 #SBATCH --mail-type=end           # send email when job ends
 #SBATCH --mail-user=sw8569@princeton.edu
-#SBATCH --output=/scratch/gpfs/sw8569/bdnn_rep_nopreds_TEST_256_%j.out
-#SBATCH --error=/scratch/gpfs/sw8569/bdnn_rep_nopreds_TEST_256_%j.err
+#SBATCH --output=/scratch/gpfs/sw8569/bdnn_syn_nopreds_TEST_256_%j.out
+#SBATCH --error=/scratch/gpfs/sw8569/bdnn_syn_nopreds_TEST_256_%j.err
 
 # Change to the directory where the script should run
 cd /scratch/gpfs/sw8569/BDNN_Arielli
@@ -18,7 +18,7 @@ module load anaconda3/2024.2
 
 # Run the analysis
 srun python ../PyRate/PyRate.py \
-    ./data/reptilia_processed_data/reptilia_pyrate_PyRate.py \
+    ./data/synapsida_processed_data/synapsida_pyrate_PyRate.py \
     -BDNNmodel 1 \
     -qShift ./data/Time_bins_ByStages.txt \
     -mG \
@@ -27,5 +27,5 @@ srun python ../PyRate/PyRate.py \
     -s 10000 \
     -p 2000 \
     -BDNNnodes 8 4 \
-    -wd ./reptilia/ \
+    -wd ./synapsida/ \
     -j 1 
