3d60a9c6ea9880f8c4b657085019c93cee441d47
shelleylwang <seashellsw@gmail.com>
2024-12-09 16:19:11 -0500

    removed combined 10 rep A_bdnn b/c of  unfinished runs 4-7 which need to be restarted. Edits to EDA b/c need to fix reverse order of env_vars

--- reptilia/mcmc_no_predictors/A_bdnn/combined_10_mcmc_cleaned_resampled.log_RTT.r
+++
@@ -1,5 +0,0 @@
-pdf(file='C:/Users/SimoesLabAdmin/Documents/BDNN_Arielli/reptilia/mcmc_no_predictors/A_bdnn/combined_10_mcmc_cleaned_resampled.log_RTT.pdf', width = 9, height = 0, useDingbats = FALSE)
-
-layout(matrix(1:0, ncol = 2, nrow = 0, byrow = TRUE))
-par(las = 1, mar = c(4.5, 4.5, 0.5, 0.5))
-dev.off()
