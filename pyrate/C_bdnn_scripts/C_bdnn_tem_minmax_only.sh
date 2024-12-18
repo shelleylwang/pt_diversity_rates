4b6708dc1350077aabc0c8b0ac20d804d6223fc7
shelleylwang <seashellsw@gmail.com>
2024-12-18 16:29:06 -0500

    modifying slurm resources of scripts, editing ltt uncertainty graph

--- pyrate/C_bdnn_scripts/C_bdnn_tem_minmax_only.sh
+++ pyrate/C_bdnn_scripts/C_bdnn_tem_minmax_only.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=C_bdnn_rep_stdscaled_only # Job name
+#SBATCH --job-name=C_bdnn_tem_minmax_only # Job name
 #SBATCH --array=1-10                     # Array with 10 independent tasks
 #SBATCH --mem-per-cpu=200MB                # Memory per CPU core (adjust if needed)
 #SBATCH --cpus-per-task=2        # cpu-cores per task (>1 if multi-threaded tasks)
@@ -7,8 +7,8 @@
 #SBATCH --mail-type=begin               # Send email when job begins
 #SBATCH --mail-type=end                 # Send email when job ends
 #SBATCH --mail-user=sw8569@princeton.edu
-#SBATCH --output=/scratch/gpfs/sw8569/C_bdnn_rep_stdscaled_only_%j_%A_%a.out
-#SBATCH --error=/scratch/gpfs/sw8569/C_bdnn_rep_stdscaled_only_%j_%A_%a.err
+#SBATCH --output=/scratch/gpfs/sw8569/C_bdnn_tem_minmax_only_%j_%A_%a.out
+#SBATCH --error=/scratch/gpfs/sw8569/C_bdnn_tem_minmax_only_%j_%A_%a.err
 #SBATCH --no-requeue                    # Disable requeue
 
 # Change to the directory where the script should run
@@ -18,7 +18,7 @@ cd /scratch/gpfs/sw8569/BDNN_Arielli
 module purge
 module load anaconda3/2024.2
 
-python ../PyRate/PyRate_thread.py ./data/reptilia_processed_data/reptilia_pyrate_PyRate.py -BDNNmodel 1 \
+python ../PyRate/PyRate_thread.py ./data/temnospondyli_processed_data/temnospondyli_pyrate_PyRate.py -BDNNmodel 1 \
 -fixShift ./data/Time_bins_ByStages.txt -qShift ./data/Time_bins_ByStages.txt -mG \
--trait_file ./data/reptilia_processed_data/reptilia_bdnn_trait_file.txt  -BDNNtimevar ./data/env_vars_data/env_vars_scaled_only.txt \
--translate -175.0 -n 100000000 -s 10000 -p 2000 -wd ./reptilia/ -seed 42 -thread 2 0 -out _C_stdscaled_only -j ${SLURM_ARRAY_TASK_ID}
+-trait_file ./data/temnospondyli_processed_data/temnospondyli_bdnn_trait_file.txt  -BDNNtimevar ./data/env_vars_data/env_vars_minmax_only.txt \
+-translate -175.0 -n 100000000 -s 10000 -p 2000 -wd ./temnospondyli/ -seed 42 -thread 2 0 -out _C_minmax_only -j ${SLURM_ARRAY_TASK_ID}
