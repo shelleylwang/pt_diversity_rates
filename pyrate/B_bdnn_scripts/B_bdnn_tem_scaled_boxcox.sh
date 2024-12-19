33529b1a95f43e625f3a87d2d8c1fd2adedf42cd
shelleylwang <seashellsw@gmail.com>
2024-12-19 18:11:17 -0500

    all scaled B_bdnn scripts, more ltt uncertainty fixes

--- pyrate/B_bdnn_scripts/B_bdnn_tem_scaled_boxcox.sh
+++ pyrate/B_bdnn_scripts/B_bdnn_tem_scaled_boxcox.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=B_bdnn_rep_stdscaled_only         # Job name
+#SBATCH --job-name=B_bdnn_tem_stdscaled_boxcox         # Job name
 #SBATCH --array=1-10                     # Array with 10 independent tasks
 #SBATCH --mem-per-cpu=300MB                # Memory per CPU core (adjust if needed)
 #SBATCH --cpus-per-task=2                # Number of CPU cores
@@ -7,8 +7,8 @@
 #SBATCH --mail-type=begin               # Send email when job begins
 #SBATCH --mail-type=end                 # Send email when job ends
 #SBATCH --mail-user=sw8569@princeton.edu
-#SBATCH --output=/scratch/gpfs/sw8569/B_bdnn_rep_stdscaled_only_%j_%A_%a.out
-#SBATCH --error=/scratch/gpfs/sw8569/B_bdnn_rep_stdscaled_only_%j_%A_%a.err
+#SBATCH --output=/scratch/gpfs/sw8569/B_bdnn_tem_stdscaled_boxcox_%j_%A_%a.out
+#SBATCH --error=/scratch/gpfs/sw8569/B_bdnn_tem_stdscaled_boxcox_%j_%A_%a.err
 #SBATCH --no-requeue                    # Disable requeue
 
 # Change to the directory where the script should run
@@ -18,8 +18,8 @@ cd /scratch/gpfs/sw8569/BDNN_Arielli
 module purge
 module load anaconda3/2024.2
 
-python ../PyRate/PyRate_thread.py ./data/reptilia_processed_data/reptilia_pyrate_PyRate.py -BDNNmodel 1 \
+python ../PyRate/PyRate_thread.py ./data/temnospondyli_processed_data/temnospondyli_pyrate_PyRate.py -BDNNmodel 1 \
 -qShift ./data/Time_bins_ByStages.txt -mG -translate -175.0 \
--trait_file ./data/reptilia_processed_data/reptilia_bdnn_trait_file.txt -BDNNtimevar ./data/1myr_temp_data/1myr_temp_scaled_only.txt \
+-trait_file ./data/temnospondyli_processed_data/temnospondyli_bdnn_trait_file.txt -BDNNtimevar ./data/1myr_temp_data/1myr_temp_scaled_boxcox.txt \
 -n 100000000 -s 10000 -p 2000 -thread 2 0 -seed 42 \
--wd ./reptilia/ -out _B_stdscaled_only -j ${SLURM_ARRAY_TASK_ID}
+-wd ./temnospondyli/ -out _B_stdscaled_boxcox -j ${SLURM_ARRAY_TASK_ID}
