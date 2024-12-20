c51e092d3cdfdbe0044ab098fe7b51c9b601b124
shelleylwang <seashellsw@gmail.com>
2024-12-20 15:56:39 -0500

    organization, D_ and A_ scripts resource edits. Adding time to C_bdnn scripts b/c old ones in cluster are at 100 hrs

--- pyrate/A_bdnn_syn_update.sh
+++ pyrate/A_bdnn_syn_update.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #SBATCH --job-name=A_bdnn_syn_update         # Job name
 #SBATCH --array=1-10                     # Array with 10 independent tasks
-#SBATCH --mem-per-cpu=1G                # Memory per CPU core (adjust if needed)
+#SBATCH --mem-per-cpu=200MB                # Memory per CPU core (adjust if needed)
+#SBATCH --cpus-per-task=2                 # Number of CPU cores per task
 #SBATCH --time=6-00:00:00               # Time limit, e.g., 1 day and 1 hour
 #SBATCH --mail-type=begin               # Send email when job begins
 #SBATCH --mail-type=end                 # Send email when job ends
@@ -17,4 +18,6 @@ cd /scratch/gpfs/sw8569/BDNN_Arielli
 module purge
 module load anaconda3/2024.2
 
-python ../PyRate/PyRate.py ./data/synapsida_processed_data/synapsida_pyrate_PyRate.py -BDNNmodel 1 -qShift ./data/Time_bins_ByStages.txt -mG -translate -175.0 -n 100000000 -s 10000 -p 2000 -BDNNnodes 4 2 -BDNNupdate_f 0.3 -wd ./synapsida/ -out _A_update -j ${SLURM_ARRAY_TASK_ID}
+python ../PyRate/PyRate_thread.py ./data/synapsida_processed_data/synapsida_pyrate_PyRate.py -BDNNmodel 1 -qShift ./data/Time_bins_ByStages.txt -mG \
+-translate -175.0 -n 100000000 -s 10000 -p 2000 -BDNNnodes 4 2 -BDNNupdate_f 0.3 -thread 2 0 \
+-wd ./synapsida/ -out _A_update -j ${SLURM_ARRAY_TASK_ID}
