b3d45812e2bedba336d273ea3813ef7ecea3db85
shelleylwang <seashellsw@gmail.com>
2024-12-20 15:56:39 -0500

    organization, D_ and A_ scripts resource edits. Adding time to C_bdnn scripts b/c old ones in cluster are at 100 hrs

--- pyrate/D_bdnn_tem.sh
+++ pyrate/D_bdnn_tem.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 #SBATCH --job-name=D_bdnn_tem         # Job name
-#SBATCH --array=0-9                     # Array with 10 independent tasks
-#SBATCH --mem-per-cpu=2G                # Memory per CPU core (adjust if needed)
-#SBATCH --time=6-00:00:00               # Time limit, e.g., 1 day and 1 hour
+#SBATCH --array=1-10                     # Array with 10 independent tasks
+#SBATCH --mem-per-cpu=200MB                # Memory per CPU core (adjust if needed)
+#SBATCH --cpus-per-task=2               # Number of CPU cores per task
+#SBATCH --time=4-00:00:00               # Time limit, e.g., 1 day and 1 hour
 #SBATCH --mail-type=begin               # Send email when job begins
 #SBATCH --mail-type=end                 # Send email when job ends
 #SBATCH --mail-user=sw8569@princeton.edu
@@ -17,19 +18,7 @@ cd /scratch/gpfs/sw8569/BDNN_Arielli
 module purge
 module load anaconda3/2024.2
 
-# Define a list of commands with varying `-j` values
-commands=(
-    "python ../PyRate/PyRate.py ./data/temnospondyli_processed_data/temnospondyli_pyrate_PyRate.py -BDNNmodel 1 -fixShift ./data/Time_bins_ByStages.txt -qShift ./data/Time_bins_ByStages.txt -mG -translate -175.0 -n 100000000 -s 10000 -p 2000 -BDNNnodes 4 2 -wd ./temnospondyli/ -out "_D" -j 1"
-    "python ../PyRate/PyRate.py ./data/temnospondyli_processed_data/temnospondyli_pyrate_PyRate.py -BDNNmodel 1 -fixShift ./data/Time_bins_ByStages.txt -qShift ./data/Time_bins_ByStages.txt -mG -translate -175.0 -n 100000000 -s 10000 -p 2000 -BDNNnodes 4 2 -wd ./temnospondyli/ -out "_D" -j 2"
-    "python ../PyRate/PyRate.py ./data/temnospondyli_processed_data/temnospondyli_pyrate_PyRate.py -BDNNmodel 1 -fixShift ./data/Time_bins_ByStages.txt -qShift ./data/Time_bins_ByStages.txt -mG -translate -175.0 -n 100000000 -s 10000 -p 2000 -BDNNnodes 4 2 -wd ./temnospondyli/ -out "_D" -j 3"
-    "python ../PyRate/PyRate.py ./data/temnospondyli_processed_data/temnospondyli_pyrate_PyRate.py -BDNNmodel 1 -fixShift ./data/Time_bins_ByStages.txt -qShift ./data/Time_bins_ByStages.txt -mG -translate -175.0 -n 100000000 -s 10000 -p 2000 -BDNNnodes 4 2 -wd ./temnospondyli/ -out "_D" -j 4"
-    "python ../PyRate/PyRate.py ./data/temnospondyli_processed_data/temnospondyli_pyrate_PyRate.py -BDNNmodel 1 -fixShift ./data/Time_bins_ByStages.txt -qShift ./data/Time_bins_ByStages.txt -mG -translate -175.0 -n 100000000 -s 10000 -p 2000 -BDNNnodes 4 2 -wd ./temnospondyli/ -out "_D" -j 5"
-    "python ../PyRate/PyRate.py ./data/temnospondyli_processed_data/temnospondyli_pyrate_PyRate.py -BDNNmodel 1 -fixShift ./data/Time_bins_ByStages.txt -qShift ./data/Time_bins_ByStages.txt -mG -translate -175.0 -n 100000000 -s 10000 -p 2000 -BDNNnodes 4 2 -wd ./temnospondyli/ -out "_D" -j 6"
-    "python ../PyRate/PyRate.py ./data/temnospondyli_processed_data/temnospondyli_pyrate_PyRate.py -BDNNmodel 1 -fixShift ./data/Time_bins_ByStages.txt -qShift ./data/Time_bins_ByStages.txt -mG -translate -175.0 -n 100000000 -s 10000 -p 2000 -BDNNnodes 4 2 -wd ./temnospondyli/ -out "_D" -j 7"
-    "python ../PyRate/PyRate.py ./data/temnospondyli_processed_data/temnospondyli_pyrate_PyRate.py -BDNNmodel 1 -fixShift ./data/Time_bins_ByStages.txt -qShift ./data/Time_bins_ByStages.txt -mG -translate -175.0 -n 100000000 -s 10000 -p 2000 -BDNNnodes 4 2 -wd ./temnospondyli/ -out "_D" -j 8"
-    "python ../PyRate/PyRate.py ./data/temnospondyli_processed_data/temnospondyli_pyrate_PyRate.py -BDNNmodel 1 -fixShift ./data/Time_bins_ByStages.txt -qShift ./data/Time_bins_ByStages.txt -mG -translate -175.0 -n 100000000 -s 10000 -p 2000 -BDNNnodes 4 2 -wd ./temnospondyli/ -out "_D" -j 9"
-    "python ../PyRate/PyRate.py ./data/temnospondyli_processed_data/temnospondyli_pyrate_PyRate.py -BDNNmodel 1 -fixShift ./data/Time_bins_ByStages.txt -qShift ./data/Time_bins_ByStages.txt -mG -translate -175.0 -n 100000000 -s 10000 -p 2000 -BDNNnodes 4 2 -wd ./temnospondyli/ -out "_D" -j 10"
-)
+python ../PyRate/PyRate_thread.py ./data/temnospondyli_processed_data/temnospondyli_pyrate_PyRate.py -BDNNmodel 1 -fixShift ./data/Time_bins_ByStages.txt -qShift ./data/Time_bins_ByStages.txt -mG \
+-translate -175.0 -n 100000000 -s 10000 -p 2000 -BDNNnodes 4 2 -thread 2 0 \
+-wd ./temnospondyli/ -out _D -j ${SLURM_ARRAY_TASK_ID}
 
-# Run the command corresponding to the current array index
-eval "${commands[$SLURM_ARRAY_TASK_ID]}"
