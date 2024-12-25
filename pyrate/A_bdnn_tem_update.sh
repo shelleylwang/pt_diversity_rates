763f72b07a19ed71fbeadf136c89fe8bb987e1da
shelleylwang <seashellsw@gmail.com>
2024-12-25 02:52:57 -0500

    last two rep scaled boxcox C runs frmo ws, edits to scripts that timed out or needed more mem on cluster

--- pyrate/A_bdnn_tem_update.sh
+++ pyrate/A_bdnn_tem_update.sh
@@ -3,7 +3,7 @@
 #SBATCH --array=1-10                     # Array with 10 independent tasks
 #SBATCH --mem-per-cpu=200MB                # Memory per CPU core (adjust if needed)
 #SBATCH --cpus-per-task=2                 # Number of CPU cores per task
-#SBATCH --time=3-00:00:00               # Time limit, e.g., 1 day and 1 hour
+#SBATCH --time=6-00:00:00               # Time limit, e.g., 1 day and 1 hour
 #SBATCH --mail-type=begin               # Send email when job begins
 #SBATCH --mail-type=end                 # Send email when job ends
 #SBATCH --mail-user=sw8569@princeton.edu
