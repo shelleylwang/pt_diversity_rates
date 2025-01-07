436f3ffadced57232bd16e3dd2c77d6b72d11e34
shelleylwang <seashellsw@gmail.com>
2025-01-07 15:52:15 -0500

    need to restore C_bdnn_rep_minmax_boxcox on the cluster

---
+++ pyrate/C_bdnn_scripts/C_bdnn_rep_minmax_boxcox_restored.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --job-name=C_bdnn_rep_minmax_boxcox_restored # Job name
+#SBATCH --array=1-10                     # Array with 10 independent tasks
+#SBATCH --mem-per-cpu=200MB                # Memory per CPU core (adjust if needed)
+#SBATCH --cpus-per-task=2        # cpu-cores per task (>1 if multi-threaded tasks)
+#SBATCH --time=6-00:00:00               # Time limit, e.g., 1 day and 1 hour
+#SBATCH --mail-type=begin               # Send email when job begins
+#SBATCH --mail-type=end                 # Send email when job ends
+#SBATCH --mail-user=sw8569@princeton.edu
+#SBATCH --output=/scratch/gpfs/sw8569/C_bdnn_rep_minmax_boxcox_restored_%j_%A_%a.out
+#SBATCH --error=/scratch/gpfs/sw8569/C_bdnn_rep_minmax_boxcox_restored_%j_%A_%a.err
+#SBATCH --no-requeue                    # Disable requeue
+
+# Change to the directory where the script should run
+cd /scratch/gpfs/sw8569/BDNN_Arielli
+
+# Load necessary modules 
+module purge
+module load anaconda3/2024.2
+
+python ../PyRate/PyRate_thread.py ./data/reptilia_processed_data/reptilia_pyrate_PyRate.py -BDNNmodel 1 \
+-restore_mcmc ./reptilia/mcmc_fixshift_predictors/C_bdnn_minmax_boxcox/reptilia_pyrate_${SLURM_ARRAY_TASK_ID}_C_minmax_boxcox_G_BDS_BDNN_16_8TVc_mcmc.log \
+-fixShift ./data/Time_bins_ByStages.txt -qShift ./data/Time_bins_ByStages.txt -mG \
+-trait_file ./data/reptilia_processed_data/reptilia_bdnn_trait_file.txt  -BDNNtimevar ./data/env_vars_data/env_vars_minmax_boxcox_restored.txt \
+-translate -175.0 -n 100000000 -s 10000 -p 2000 -wd ./reptilia/mcmc_fixshift_predictors/C_bdnn_minmax_boxcox/ -seed 42 -thread 2 0 -out _C_minmax_boxcox_restored -j ${SLURM_ARRAY_TASK_ID}
