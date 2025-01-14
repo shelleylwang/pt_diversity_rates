8e1c8733fd42755323b1456d4cb998dcadd1f4bb
shelleylwang <seashellsw@gmail.com>
2025-01-14 17:29:02 -0500

    shifting mbd preds by 175, adding A_bdnn_update_restored logs from cluster, removing -thread from B_bdnn scripts

--- pyrate/A_bdnn_rep_update_restored.sh
+++ pyrate/A_bdnn_rep_update_restored.sh
@@ -21,5 +21,5 @@ module load anaconda3/2024.2
 python ../PyRate/PyRate.py ./data/reptilia_processed_data/reptilia_pyrate_PyRate.py \
 -restore_mcmc ./reptilia/mcmc_no_predictors/A_bdnn_update/reptilia_pyrate_${SLURM_ARRAY_TASK_ID}_A_update_G_BDS_BDNN_4_2Tc_mcmc.log  \
 -BDNNmodel 1 -qShift ./data/Time_bins_ByStages.txt -mG \
--translate -175.0 -n 100000000 -s 10000 -p 2000 -BDNNnodes 4 2 -BDNNupdate_f 0.3 -thread 2 0 \
+-translate -175.0 -n 100000000 -s 10000 -p 2000 -BDNNnodes 4 2 -BDNNupdate_f 0.3 \
 -wd ./reptilia/mcmc_no_predictors/A_bdnn_update/A_bdnn_update_restored_logs/ -out _A_update_restored -j ${SLURM_ARRAY_TASK_ID}
