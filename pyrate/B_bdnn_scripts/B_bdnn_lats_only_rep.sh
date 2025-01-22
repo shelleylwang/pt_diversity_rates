331c06b5ebba616a5470933809b42e6fd1169e47
shelleylwang <seashellsw@gmail.com>
2025-01-22 10:52:19 -0500

    removing multi-core req from scripts

--- pyrate/B_bdnn_scripts/B_bdnn_lats_only_rep.sh
+++ pyrate/B_bdnn_scripts/B_bdnn_lats_only_rep.sh
@@ -2,7 +2,6 @@
 #SBATCH --job-name=B_bdnn_lats_only_rep        # Job name
 #SBATCH --array=1-10                     # Array with 10 independent tasks
 #SBATCH --mem-per-cpu=300MB                # Memory per CPU core (adjust if needed)
-#SBATCH --cpus-per-task=2                # Number of CPU cores
 #SBATCH --time=6-00:00:00               # Time limit, e.g., 1 day and 1 hour
 #SBATCH --mail-type=begin               # Send email when job begins
 #SBATCH --mail-type=end                 # Send email when job ends
@@ -22,4 +21,4 @@ python ../PyRate/PyRate.py ./data/reptilia_processed_data/reptilia_pyrate_PyRate
 -qShift ./data/Time_bins_ByStages.txt -mG -translate -175.0 \
 -trait_file ./data/reptilia_processed_data/reptilia_bdnn_trait_file.txt \
 -n 100000000 -s 10000 -p 2000 -BDNNnodes 4 2 -seed 42 \
--wd ./reptilia/ -out _B_lats_only-j ${SLURM_ARRAY_TASK_ID}
+-wd ./reptilia/ -out _B_lats_only -j ${SLURM_ARRAY_TASK_ID}
