c95a0c5b9c312e6b2212f9a2e82440bfe6200956
shelleylwang <seashellsw@gmail.com>
2025-05-27 16:31:39 -0400

    all pyrate .py input files created for pt and gl

--- updated_occurrence_analyses/pyrate_prep.R
+++ updated_occurrence_analyses/pyrate_prep.R
@@ -2,7 +2,13 @@ setwd("C:/Users/SimoesLabAdmin/Documents/BDNN_Arielli")
 source("../PyRate/pyrate_utilities.r")
 
 # Note that the input .txts need to have columns that are in the exact order specified by pyrate_utilities.r in order to work: "Species", "Status", "min_age", "max_age"
+
+# PERMIAN-TRIASSIC
 extract.ages(file="updated_occurrence_analyses/data/Perm-Trias/pt_reptilia_all_filtered_spellchecked_renamed.txt", replicates=10)
 extract.ages(file="updated_occurrence_analyses/data/Perm-Trias/pt_reptilia_terr_filtered_spellchecked_renamed.txt", replicates=10)
 extract.ages(file="updated_occurrence_analyses/data/Perm-Trias/pt_synapsida_filtered_spellchecked_renamed.txt", replicates=10)
 
+
+# GUADALUPIAN-LADINIAN
+extract.ages(file="updated_occurrence_analyses/data/Guad-Ladin/gl_reptilia_terr_epochs_filtered.txt", replicates=10)
+extract.ages(file="updated_occurrence_analyses/data/Guad-Ladin/gl_synapsida_epochs_filtered.txt", replicates=10)
