0ae0de432366262885502ec9bd68d8166b1a1f26
shelleylwang <seashellsw@gmail.com>
2025-05-27 15:31:33 -0400

    finished cleaning, created pyrate input files

--- updated_occurrence_analyses/pyrate_prep.R
+++ updated_occurrence_analyses/pyrate_prep.R
@@ -1,3 +1,8 @@
-source("PyRate/pyrate_utilities.r")
+setwd("C:/Users/SimoesLabAdmin/Documents/BDNN_Arielli")
+source("../PyRate/pyrate_utilities.r")
 
-extract.ages(file="updated_occurrence_analyses/data/Perm-Trias/")
+# Note that the input .txts need to have columns that are in the exact order specified by pyrate_utilities.r in order to work: "Species", "Status", "min_age", "max_age"
+extract.ages(file="updated_occurrence_analyses/data/Perm-Trias/pt_reptilia_all_filtered_spellchecked_renamed.txt", replicates=10)
+extract.ages(file="updated_occurrence_analyses/data/Perm-Trias/pt_reptilia_terr_filtered_spellchecked_renamed.txt", replicates=10)
+extract.ages(file="updated_occurrence_analyses/data/Perm-Trias/pt_synapsida_filtered_spellchecked_renamed.txt", replicates=10)
+
