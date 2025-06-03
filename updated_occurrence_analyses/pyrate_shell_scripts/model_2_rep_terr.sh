4b4ca15c03b35076d34894d49cd592fdc7b02404
shelleylwang <seashellsw@gmail.com>
2025-06-03 16:08:35 -0400

    all no predictor model shell scripts re-written

--- updated_occurrence_analyses/pyrate_shell_scripts/model_2_rep_terr.sh
+++ updated_occurrence_analyses/pyrate_shell_scripts/model_2_rep_terr.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --job-name=model_2_rep_terr          # Job name
 #SBATCH --array=1-10                     # Array with 10 independent tasks
-#SBATCH --mem-per-cpu=400MB                # Memory per CPU core (adjust if needed)
-#SBATCH --time=6-00:00:00               # Time limit, e.g., 1 day and 1 hour
+#SBATCH --mem-per-cpu=200MB                # Memory per CPU core (adjust if needed)
+#SBATCH --time=2-00:00:00               # Time limit, e.g., 1 day and 1 hour
 #SBATCH --mail-type=begin               # Send email when job begins
 #SBATCH --mail-type=end                 # Send email when job ends
 #SBATCH --mail-user=sw8569@princeton.edu
@@ -18,6 +18,6 @@ module purge
 module load anaconda3/2024.2
 
 # Define command to run
-python ../PyRate/PyRate.py updated_occurrence_analyses/data/Perm-Trias/pt_reptilia_terr_filtered_spellchecked_renamed_PyRate.py \
--A 4 -qShift updated_occurrence_analyses/data/Perm-Trias/Time_bins_ByStages.txt -mG \
--n 100000000 -s 10000 -wd updated_occurrence_analyses/model_2_and_5/reptilia_terr -out _model_2 -j ${SLURM_ARRAY_TASK_ID} 
+python ../PyRate/PyRate.py updated_occurrence_analyses/data/Perm-Trias/pt_reptilia_terr_pyrate_input_txt_PyRate.py \
+ -A 4 -qShift updated_occurrence_analyses/data/Perm-Trias/Time_bins_ByStages.txt -mG \
+ -n 100000000 -s 10000 -wd updated_occurrence_analyses/model_2_and_5/reptilia_terr -out _model_2 -j ${SLURM_ARRAY_TASK_ID} 
