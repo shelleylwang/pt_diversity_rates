0611d8053912832042fb744e784444e18744e9e3
shelleylwang <seashellsw@gmail.com>
2025-06-16 17:16:46 -0400

    edits to models 3 4 and 6 for resubmission

--- updated_occurrence_analyses/pyrate_shell_scripts/model_6_rep_all.sh
+++ updated_occurrence_analyses/pyrate_shell_scripts/model_6_rep_all.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --job-name=model_6_rep_all          # Job name
 #SBATCH --array=1-10                     # Array with 10 independent tasks
-#SBATCH --mem-per-cpu=400MB                # Memory per CPU core (adjust if needed)
-#SBATCH --time=6-00:00:00               # Time limit, e.g., 1 day and 1 hour
+#SBATCH --mem-per-cpu=200MB                # Memory per CPU core (adjust if needed)
+#SBATCH --time=4-00:00:00               # Time limit, e.g., 1 day and 1 hour
 #SBATCH --mail-type=begin               # Send email when job begins
 #SBATCH --mail-type=end                 # Send email when job ends
 #SBATCH --mail-user=sw8569@princeton.edu
@@ -21,11 +21,10 @@ module load anaconda3/2024.2
 python ../PyRate/PyRate.py updated_occurrence_analyses/data/Guad-Ladin/gl_reptilia_all_epochs_filtered_PyRate.py \
  -BDNNmodel 1 -fixShift updated_occurrence_analyses/data/Guad-Ladin/Time_bins_ByStages.txt \
  -qShift updated_occurrence_analyses/data/Guad-Ladin/Time_bins_ByStages.txt -mG \
- -trait_file updated_occurrence_analyses/data/Perm-Trias/bdnn_trait_files/reptilia_all_lats_file_groupby_filtered.txt \
+ -trait_file updated_occurrence_analyses/data/Guad-Ladin/bdnn_trait_files/gl_reptilia_all_lats_file_final.txt \
  -BDNNtimevar updated_occurrence_analyses/data/Guad-Ladin/songEA_isotopic_data/songEA_data_ordered_filtered.txt -translate 175 \
  -out _model_6 -wd updated_occurrence_analyses/model_6/reptilia_all -n 200000000 -s 10000 -j ${SLURM_ARRAY_TASK_ID} 
 
 
 
 
-#  python ../PyRate/PyRate.py updated_occurrence_analyses/data/Guad-Ladin/gl_reptilia_all_epochs_filtered_PyRate.py -BDNNmodel 1 -fixShift updated_occurrence_analyses/data/Guad-Ladin/Time_bins_ByStages.txt -qShift updated_occurrence_analyses/data/Guad-Ladin/Time_bins_ByStages.txt -mG -trait_file updated_occurrence_analyses/data/Perm-Trias/bdnn_trait_files/reptilia_all_lats_file_groupby_filtered.txt -BDNNtimevar updated_occurrence_analyses/data/Guad-Ladin/songEA_isotopic_data/songEA_data_ordered_filtered.txt -translate 175 -out _model_6 -wd updated_occurrence_analyses/model_6/reptilia_all -n 200000000 -s 10000 -j ${SLURM_ARRAY_TASK_ID} 
