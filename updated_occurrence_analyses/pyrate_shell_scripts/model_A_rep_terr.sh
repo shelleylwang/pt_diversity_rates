795783ec5450d1a2a1b3fc57e88bdb6b9fba2fbf
Shelley Wang <seashellsw@gmail.com>
2025-07-18 14:01:12 -0400

    tuning models, scripts for submission

--- updated_occurrence_analyses/pyrate_shell_scripts/model_A_rep_terr.sh
+++ updated_occurrence_analyses/pyrate_shell_scripts/model_A_rep_terr.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
-#SBATCH --job-name=model_1_rep_terr          # Job name
+#SBATCH --job-name=model_A_rep_terr         # Job name
 #SBATCH --array=1-10                     # Array with 10 independent tasks
-#SBATCH --mem-per-cpu=400MB                # Memory per CPU core (adjust if needed)
-#SBATCH --time=6-00:00:00               # Time limit, e.g., 1 day and 1 hour
+#SBATCH --mem-per-cpu=200MB                # Memory per CPU core (adjust if needed)
+#SBATCH --time=4-00:00:00               # Time limit, e.g., 1 day and 1 hour
 #SBATCH --mail-type=begin               # Send email when job begins
 #SBATCH --mail-type=end                 # Send email when job ends
 #SBATCH --mail-user=sw8569@princeton.edu
-#SBATCH --output=/scratch/gpfs/sw8569/model_1_%j_%A_%a.out
-#SBATCH --error=/scratch/gpfs/sw8569/model_1_%j_%A_%a.err
+#SBATCH --output=/scratch/gpfs/sw8569/model_A_rep_terr_%j_%A_%a.out
+#SBATCH --error=/scratch/gpfs/sw8569/model_A_rep_terr_%j_%A_%a.err
 #SBATCH --no-requeue                    # Disable requeue
-
-# NOTE: This script SHOULD HAVE been named model_A instead of model_1
-# So every instance of "model_1" in the script should be changed to "model_A"
-# To match the "additional models" in the analyses log
 
 # Change to the directory where the script should run
 cd /scratch/gpfs/sw8569/BDNN_Arielli
@@ -22,7 +18,12 @@ module purge
 module load anaconda3/2024.2
 
 # Define command to run
-python ../PyRate/PyRate.py updated_occurrence_analyses/data/Perm-Trias/pt_reptilia_terr_filtered_spellchecked_renamed_PyRate.py \
--BDNNmodel 1 -A 0 -qShift updated_occurrence_analyses/data/Perm-Trias/Time_bins_ByStages.txt -mG \
--translate 175 -fixShift updated_occurrence_analyses/data/Perm-Trias/Time_bins_ByStages.txt \
- -n 100000000 -s 10000 -wd updated_occurrence_analyses/model_1/reptilia_terr -out _model_1 -j ${SLURM_ARRAY_TASK_ID} 
+
+python ../PyRate/PyRate.py updated_occurrence_analyses/data/Perm-Trias/pt_reptilia_terr_pyrate_input_txt_PyRate.py \
+ -BDNNmodel 1 -fixShift updated_occurrence_analyses/data/Perm-Trias/Time_bins_ByStages.txt \
+ -qShift updated_occurrence_analyses/data/Perm-Trias/Time_bins_ByStages.txt -mG \
+ -trait_file updated_occurrence_analyses/data/Perm-Trias/bdnn_trait_files/pt_reptilia_terr_lats_file_final.txt \
+ -BDNNtimevar updated_occurrence_analyses/data/AF_climactic_data/reptilia_terr_BRIDGE_z_trans_filtered.txt -translate -175 \
+ -edgeShift 274.4 237 \
+ -out _model_A -wd updated_occurrence_analyses/model_A/reptilia_terr -n 200000000 -s 10000 -j ${SLURM_ARRAY_TASK_ID}
+
