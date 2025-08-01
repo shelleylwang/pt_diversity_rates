5e2d025856638d717f55d02ddb9e4af23af013c0
shelleylwang <seashellsw@gmail.com>
2025-07-31 21:03:24 -0400

    had to extend youngest isotopic data to beginning of binary stages time in order for program to run. models 8 and 9 scripts

---
+++ updated_occurrence_analyses/pyrate_shell_scripts/model_8_rep_all.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH --job-name=model_8_rep_all          # Job name
+#SBATCH --array=1-10                     # Array with 10 independent tasks
+#SBATCH --mem-per-cpu=300MB                # Memory per CPU core (adjust if needed)
+#SBATCH --time=6-00:00:00               # Time limit, e.g., 1 day and 1 hour
+#SBATCH --mail-type=begin               # Send email when job begins
+#SBATCH --mail-type=end                 # Send email when job ends
+#SBATCH --mail-user=sw8569@princeton.edu
+#SBATCH --output=/scratch/gpfs/sw8569/model_8_rep_all_%j_%A_%a.out
+#SBATCH --error=/scratch/gpfs/sw8569/model_8_rep_all_%j_%A_%a.err
+#SBATCH --no-requeue                    # Disable requeue
+
+# Change to the directory where the script should run
+cd /scratch/gpfs/sw8569/BDNN_Arielli
+
+# Load necessary modules
+module purge
+module load anaconda3/2024.2
+
+# Define command to run
+python ../PyRate/PyRate.py updated_occurrence_analyses/data/Perm-Trias/pt_reptilia_all_pyrate_input_txt_PyRate.py  \
+ -BDNNmodel 1 -fixShift updated_occurrence_analyses/data/Perm-Trias/Time_bins_1myr_and_stages.txt \
+ -qShift updated_occurrence_analyses/data/Perm-Trias/Time_bins_ByStages.txt -mG \
+ -trait_file updated_occurrence_analyses/data/Perm-Trias/bdnn_trait_files/pt_reptilia_all_lats_file_final.txt \
+ -BDNNtimevar updated_occurrence_analyses/data/Guad-Ladin/songEA_isotopic_data/isotopic_and_binary_stages_data/isotopic_stages_timevar_1myr.txt \
+ -BDNNtimetrait 0 -translate -175 -out _model_8 -wd updated_occurrence_analyses/model_8/reptilia_all -n 200000000 -s 1000 -j ${SLURM_ARRAY_TASK_ID} 
+
+
+
