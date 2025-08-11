0144166b499a92d21f97e03677ea38c38c7a439e
shelleylwang <seashellsw@gmail.com>
2025-08-11 11:37:16 -0400

    fixing a pdp

--- updated_occurrence_analyses/updated_occs_coda_calls.R
+++ updated_occurrence_analyses/updated_occs_coda_calls.R
@@ -3,8 +3,8 @@ setwd("C:/Users/SimoesLabAdmin/Documents/pt_diversity_rates")
 source("mcmc_logs_analyses_code/coda_final_2.R")
 source("mcmc_logs_analyses_code/coda_ess_diagnostics.R")
 
-analyze_ess_diagnostics("C:\\Users\\SimoesLabAdmin\\Downloads\\")
-analyze_mcmc("C:\\Users\\SimoesLabAdmin\\Downloads")
+analyze_ess_diagnostics("C:\\Users\\SimoesLabAdmin\\Downloads\\reptilia_all")
+analyze_mcmc("C:\\Users\\SimoesLabAdmin\\Downloads\\reptilia_all")
 
 
 ############## MODEL 1
