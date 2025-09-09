a0aafc0685cc33c4dc3a97a45f7315088ded76ab
shelleylwang <seashellsw@gmail.com>
2025-09-09 10:21:04 -0400

    offset iso data unneeded b/c stage pdp plot in truncated pdps uneeded

--- updated_occurrence_analyses/updated_occs_coda_calls.R
+++ updated_occurrence_analyses/updated_occs_coda_calls.R
@@ -3,9 +3,6 @@ setwd("C:/Users/SimoesLabAdmin/Documents/pt_diversity_rates")
 source("mcmc_logs_analyses_code/coda_final_2.R")
 source("mcmc_logs_analyses_code/coda_ess_diagnostics.R")
 
-# analyze_ess_diagnostics("C:\\Users\\SimoesLabAdmin\\Downloads\\reptilia_all")
-# analyze_mcmc("C:\\Users\\SimoesLabAdmin\\Downloads\\reptilia_all")
+analyze_ess_diagnostics("C:\\Users\\SimoesLabAdmin\\Downloads")
+analyze_mcmc("C:\\Users\\SimoesLabAdmin\\Downloads")
 
-
-# analyze_ess_diagnostics("C:\\Users\\SimoesLabAdmin\\Documents\\pt_diversity_rates\\updated_occurrence_analyses\\model_7\\isotopic_pt_1myr\\synapsida")
-analyze_mcmc("C:\\Users\\SimoesLabAdmin\\Documents\\pt_diversity_rates\\updated_occurrence_analyses\\model_1\\reptilia_terr")
