e1eeaa80504cff6a1c22d1a63997057abc45d690
shelleylwang <seashellsw@gmail.com>
2025-09-18 15:00:00 -0400

    model 9 syn files from della cluster and model 8 syn offset (syn rerun) and model 9 rep post processing. Also reducing sampilng rate to 1k on models 2 and 5 scripts to try and improve diagnostics

--- updated_occurrence_analyses/updated_occs_coda_calls.R
+++ updated_occurrence_analyses/updated_occs_coda_calls.R
@@ -3,6 +3,6 @@ setwd("C:/Users/SimoesLabAdmin/Documents/pt_diversity_rates")
 source("mcmc_logs_analyses_code/coda_final_2.R")
 source("mcmc_logs_analyses_code/coda_ess_diagnostics.R")
 
-analyze_ess_diagnostics("C:\\Users\\SimoesLabAdmin\\Documents\\pt_diversity_rates\\updated_occurrence_analyses\\model_9\\reptilia_terr_offset")
-analyze_mcmc("C:\\Users\\SimoesLabAdmin\\Documents\\pt_diversity_rates\\updated_occurrence_analyses\\model_9\\reptilia_terr_offset")
+analyze_ess_diagnostics("C:\\Users\\SimoesLabAdmin\\Documents\\pt_diversity_rates\\updated_occurrence_analyses\\model_9\\synapsida_offset")
+analyze_mcmc("C:\\Users\\SimoesLabAdmin\\Documents\\pt_diversity_rates\\updated_occurrence_analyses\\model_9\\synapsida_offset")
 
