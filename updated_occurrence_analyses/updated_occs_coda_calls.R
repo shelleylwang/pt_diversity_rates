87c24dbd42780382b1131b2a530f16274a6e72e4
shelleylwang <seashellsw@gmail.com>
2025-09-19 16:13:12 -0400

    redoing models 3, 7, 8, 9 diagnostics with new coda formatting, model 9 synapsida truncated pdps

--- updated_occurrence_analyses/updated_occs_coda_calls.R
+++ updated_occurrence_analyses/updated_occs_coda_calls.R
@@ -4,5 +4,6 @@ source("mcmc_logs_analyses_code/coda_final_2.R")
 source("mcmc_logs_analyses_code/coda_ess_diagnostics.R")
 
 # analyze_ess_diagnostics("C:\\Users\\SimoesLabAdmin\\Documents\\pt_diversity_rates\\updated_occurrence_analyses\\model_9\\reptilia_terr_offset")
-analyze_mcmc("C:\\Users\\SimoesLabAdmin\\Documents\\pt_diversity_rates\\updated_occurrence_analyses\\model_9\\reptilia_terr_offset")
+analyze_mcmc("C:\\Users\\SimoesLabAdmin\\Documents\\pt_diversity_rates\\updated_occurrence_analyses\\model_9\\reptilia_terr")
+
 
