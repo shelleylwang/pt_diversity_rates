fb067dda243c2d02fd52d9ebd141bd71de1b35f0
shelleylwang <seashellsw@gmail.com>
2025-09-22 13:51:37 -0400

    model 9 duplicate files from other cluster, rep 8 from della is incomplete, test s1k for model 2 synapsida shows good diagnostics but graph results are about the same as before

--- updated_occurrence_analyses/updated_occs_coda_calls.R
+++ updated_occurrence_analyses/updated_occs_coda_calls.R
@@ -3,7 +3,7 @@ setwd("C:/Users/SimoesLabAdmin/Documents/pt_diversity_rates")
 source("mcmc_logs_analyses_code/coda_final_2.R")
 source("mcmc_logs_analyses_code/coda_ess_diagnostics.R")
 
-# analyze_ess_diagnostics("C:\\Users\\SimoesLabAdmin\\Documents\\pt_diversity_rates\\updated_occurrence_analyses\\model_9\\reptilia_terr_offset")
-analyze_mcmc("C:\\Users\\SimoesLabAdmin\\Documents\\pt_diversity_rates\\updated_occurrence_analyses\\model_9\\reptilia_terr")
+analyze_ess_diagnostics("C:\\Users\\SimoesLabAdmin\\Documents\\pt_diversity_rates\\updated_occurrence_analyses\\model_8\\reptilia_all")
+analyze_mcmc("C:\\Users\\SimoesLabAdmin\\Documents\\pt_diversity_rates\\updated_occurrence_analyses\\model_8\\reptilia_all")
 
 
