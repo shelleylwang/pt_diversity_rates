097b41aff9d7f03e5f918f22a6a10e3bd7719c5f
shelleylwang <seashellsw@gmail.com>
2025-12-19 16:45:23 -0500

    new model 9 dates post processing

--- reusable_code/mcmc_logs_analyses_code/updated_occs_coda_calls_function.R
+++ reusable_code/mcmc_logs_analyses_code/updated_occs_coda_calls_function.R
@@ -1,7 +1,7 @@
 setwd("C:/Users/SimoesLabAdmin/Documents/pt_diversity_rates")
 #setwd('/Volumes/My Passport/pt_diversity_rates')
 source("reusable_code/mcmc_logs_analyses_code/coda_final_2.R")
-source("reusable_code/mcmc_logs_analyses_code/coda_ess_diagnostics.R")
+# source("reusable_code/mcmc_logs_analyses_code/coda_ess_diagnostics.R")
 
-analyze_ess_diagnostics("C:\\Users\\SimoesLabAdmin\\Documents\\pt_diversity_rates\\updated_occurrence_analyses\\model_9\\reptilia_terr_new_dates")
-analyze_mcmc("C:\\Users\\SimoesLabAdmin\\Documents\\pt_diversity_rates\\updated_occurrence_analyses\\model_9\\reptilia_terr_new_dates")
+# analyze_ess_diagnostics("C:\\Users\\SimoesLabAdmin\\Documents\\pt_diversity_rates\\updated_occurrence_analyses\\model_9\\synapsida_new_dates")
+analyze_mcmc("C:\\Users\\SimoesLabAdmin\\Documents\\pt_diversity_rates\\updated_occurrence_analyses\\model_9\\synapsida_new_dates")
